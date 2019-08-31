6cf75f493faf8dad3a7941fc5a6f51d845b1ad59
yuzrain <yuzairun@126.com>
2019-08-31 09:39:59 +0800

    toggle the LED

--- applications/test_ble/application.c
+++ applications/test_ble/application.c
@@ -13,12 +13,47 @@
  * @addtogroup NRF52832
  */
 /*@{*/
-
 #include <rtthread.h>
+#include <rtdevice.h>
+#include <rthw.h>
+
 
 #ifdef __GNUC__
 
-	/* We do nothing */
+void main_tm_task(void *parameter)
+{
+
+}
+
+void _main(void)
+{
+	uint8_t flag=0;
+	rt_pin_mode(17, PIN_MODE_OUTPUT);
+	rt_pin_mode(18, PIN_MODE_OUTPUT);
+	rt_pin_mode(19, PIN_MODE_OUTPUT);
+	rt_pin_mode(20, PIN_MODE_OUTPUT);
+	//rt_timer_create("main_tm", main_tm_task, NULL, 100, RT_TIMER_FLAG_PERIODIC|RT_TIMER_FLAG_SOFT_TIMER);
+	while (1)
+	{
+		rt_thread_delay(100);
+		if (flag == 0)
+		{
+			rt_pin_write(17, 1);
+			rt_pin_write(18, 1);
+			rt_pin_write(19, 1);
+			rt_pin_write(20, 1);
+			flag = 1;
+		}
+		else
+		{
+			rt_pin_write(17, 0);
+			rt_pin_write(18, 0);
+			rt_pin_write(19, 0);
+			rt_pin_write(20, 0);
+			flag = 0;
+		}
+	}
+}
 
 #else
 
